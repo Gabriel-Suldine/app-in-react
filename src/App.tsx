50585ff3b2de3e107c15d6b1feff8439710b2175
Gabriel-Suldine <gabriel.suldinne@gmail.com>
2023-03-09 21:02:24 -0400

    Preparando o componente Gallery

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Login } from './components/pages/Login'
 import { Register } from './components/pages/Register'
 import { NavBarMenu } from './components/NavBarMenu/index'
 import { StateExemple } from './components/pages/StateExemple'
+import { Gallery } from './components/Gallery'
 
 
 function App() {
   return (
     <div id="App">
+      <Gallery/> 
       <StateExemple />
     </div>
   )
--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
 import { movies } from "./data"
-export function Gallery(){
-    return(
+import './style.css'
+export function Gallery() {
+    const [index, setIndex] = useState(0);
+
+    let movie = movies[index]
+    function handleClickMore() {
+        if (index >= 14) {
+            setIndex(14)
+        } else {
+            setIndex(index + 1)
+        }
+    }
+    function handleClickLess() {
+        if (index <= 0) {
+            setIndex(0)
+        } else {
+            setIndex(index - 1)
+        }
+    }
+    return (
         <>
-        <section className="gallery">
-        <img src=""/>            
-        </section>
-        <button>Anterior</button>
-        <button>Próxima</button>
-        
+        <div className="centralizar">
+            <section className="gallery">
+                <img src={movie.images.poster} />
+            </section>
+
+            <div className="details">
+                <h1>{movie.title}</h1>
+                <details>
+                    <summary>Descrição</summary>
+                    <p>{movie.synopsis}</p>
+                </details>
+                <p>DURAÇÃO: {movie.duration}</p>
+                <p>CLASSIFICAÇÃO DE IDADE: {movie.parentalRating}</p>
+            </div>
+
+            <div className="buttons">
+                <button onClick={handleClickLess} >Anterior</button>
+                <button onClick={handleClickMore}>Próxima</button>
+            </div>
+
+            </div>
         </>
     )
 }
+
+
